9e41a66e8396fc70d80d9f8c9128a6c84918b64c
Wesley Hughes <wesley_hughes@me.com>
2023-04-24 12:45:49 -0500

    home page optimized for mobile

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,8 +2,8 @@ export const Home = () => {
   return (
     <>
 
-      <a href="/" class="flex flex-row mx-auto my-[150px] items-center justify-around bg-white w-[75%] border h-[450px] border-gray-200 rounded-lg shadow  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <div class="flex flex-col justify-between p-4 leading-normal">
+      <a href="/" class="flex flex-col mx-auto my-[150px] items-center justify-around bg-white w-[75%] md:flex-row h-[450px] border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <div class="flex flex-col object-cover justify-between p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Hello, I'm Wesley Hughes.</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Embracing challenges and finding new solutions.</p>
     </div>
